b7a0d1fe866594884036f065ec3b39ddd6341d94
Rex Sorgatz <rexsorgatz@gmail.com>
2016-05-02 16:49:11 -0400

    Removing HP TV Tattle box

--- blocks/featured_block.php
+++ blocks/featured_block.php
@@ -34,36 +34,6 @@
 			<ul class="show-list">
 				<li>
 					<article class="show">
-						<div class="featured-header"><a href="http://www.tvinsider.com/author/norman-weiss/"><img src="http://www.tvinsider.com/wp-content/themes/tvinsider/images/tv_tattle_header.png" width="201" height="107" /></a></div>
-							<?php if($_tattle_posts) { $x = $_tattle_posts; } else { $x = $t; }; ?>
-								<?php foreach($x as $post): ?>
-									<?php setup_postdata($post); ?>
-										<?php if(has_post_thumbnail($post->ID)): ?>
-										<div class="img-holder">
-										<a href="<?php echo get_permalink($post->ID); ?>">
-										<?php 
-											if (MultiPostThumbnails::has_post_thumbnail(get_post_type($post->ID),'promo-small', $post->ID)) { 
-												MultiPostThumbnails::the_post_thumbnail(get_post_type($post->ID),'promo-small', $post->ID, 't_330x240');
-											} else { 
-												echo get_the_post_thumbnail($post->ID, 't_330x240');
-											}; 
-										?>
-										</a>
-										</div>
-										<div class="text">
-											<h3><a href="<?php echo get_permalink($post->ID); ?>"><?php $subhead = get_field('short_headline', $post->ID); if($subhead) {echo strip_tags(apply_filters('the_content', $subhead),"<i>");} else {echo get_the_title($post->ID);}?></a></h3>
-											<?php
-												if($post->post_excerpt) echo apply_filters('the_excerpt', $post->post_excerpt);
-												elseif($post->post_content) echo string_limit_words(apply_filters('the_excerpt', $post->post_content), 18);
-											?>
-										</div>
-										<?php endif; ?>
-								<?php endforeach; ?>
-						<?php wp_reset_postdata(); ?>
-					</article>
-				</li>
-				<li>
-					<article class="show">
 						<div class="featured-header"><a href="http://www.tvinsider.com/whats-worth-watching/"><img src="http://www.tvinsider.com/wp-content/themes/tvinsider/images/whats_worth_watching_header.png" width="285" height="107" /></a></div>
 							<?php if($_watching_posts) { $x = $_watching_posts; } else { $x = $w; }; ?>
 								<?php foreach($x as $post): ?>
